5878865e0ad8aa0bb04bf2aed82828224f13eaf6
29449 <2944972991@qq.com>
2021-08-17 15:18:43 +0800

    等待更新随机数限制范围方法

--- contracts/RandomNumberConsumer.sol
+++ contracts/RandomNumberConsumer.sol
@@ -16,6 +16,9 @@ contract RandomNumberConsumer is VRFConsumerBase {
     uint256 internal randomResult;
     mapping(address => uint256) internal userRandom;
     mapping(bytes32 => address) internal requestIdToAddress;
+    uint256 internal x;
+    uint256 internal y;
+
 
 
     /**
@@ -23,15 +26,23 @@ contract RandomNumberConsumer is VRFConsumerBase {
      * VRF地址 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
      * LINK地址 0xa36085F69e2889c224210F603D836748e7dC0088
      * KeyHash 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     * _x x人领取
+     * _y y输出量
      */
-    constructor()
+    constructor(
+        uint256 _x,
+        uint256 _y
+    )
     VRFConsumerBase(
         0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
         0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
     )
     {
+        x = _x;
+        y = _y;
         keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
         fee = 0.1 * 10 ** 18;
+
     }
 
     /**
@@ -39,7 +50,7 @@ contract RandomNumberConsumer is VRFConsumerBase {
      * @param userAddress 用户地址
      * @return requestId 用于验证的id
      */
-    function getRandomNumber(address userAddress) public returns (bytes32 requestId) {
+    function getRandomNumber(address userAddress) external returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK");
         requestId = requestRandomness(keyHash, fee);
         requestIdToAddress[requestId] = userAddress;
@@ -52,7 +63,8 @@ contract RandomNumberConsumer is VRFConsumerBase {
      * @param randomness 得到的随机数
      */
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = (randomness % 50) + 1;
+        uint256 limit = randomLimit();
+        randomResult = (randomness % limit) + 1;
         userRandom[requestIdToAddress[requestId]] = randomResult;
 
     }
@@ -62,7 +74,7 @@ contract RandomNumberConsumer is VRFConsumerBase {
     * @param n 得到随机数的数量
     * @return expandedValues n个随机数的数组
     */
-    function expand(uint256 n) public view returns (uint256[] memory expandedValues) {
+    function expand(uint256 n) external view returns (uint256[] memory expandedValues) {
         expandedValues = new uint256[](n);
         for (uint256 i = 0; i < n; i++) {
             expandedValues[i] = (uint256(keccak256(abi.encode(randomResult, i))) % 50) + 1;
@@ -75,8 +87,15 @@ contract RandomNumberConsumer is VRFConsumerBase {
     * @param userAddress 用户地址
     * @return random 随机数
     */
-    function getUserRandom(address userAddress) public view returns (uint256 random) {
+    function getUserRandom(address userAddress) external view returns (uint256 random) {
         return userRandom[userAddress];
+    }
+
+    /**
+    * @dev !!!随机数生成限制!!!
+    */
+    function randomLimit() private view returns (uint256){
+        return x;
     }
 }
 
@@ -90,18 +109,22 @@ contract GrabARedEnvelope is Ownable {
     /**
     * @dev 合约部署
     * @param _tokenAddress token地址
+    * @param _x x为参与人数
+    * @param _y y为输出总数
     */
     constructor(
-        ERC20 _tokenAddress
+        ERC20 _tokenAddress,
+        uint256 _x,
+        uint256 _y
     ){
-        randomNumberConsumer = new RandomNumberConsumer();
+        randomNumberConsumer = new RandomNumberConsumer(_x, _y);
         tokenAddress = _tokenAddress;
     }
 
     /**
     * @dev 用户领取，给用户生成相应的随机数，将延时获得
     */
-    function userReceive() public {
+    function userReceive() external {
         randomNumberConsumer.getRandomNumber(msg.sender);
         userCount++;
     }
@@ -126,7 +149,7 @@ contract GrabARedEnvelope is Ownable {
     /**
     * @dev 查看领取用户数量
     */
-    function getReceivingUsers() public view returns (uint256){
+    function getReceivingUsers() external view returns (uint256){
         return userCount;
     }
 
@@ -135,7 +158,7 @@ contract GrabARedEnvelope is Ownable {
     * @param _userAddress 借用用户地址
     * @param _count 借用数量
     */
-    function borrowMoney(ERC20 _tokenAddress, address _userAddress, uint256 _count) public onlyOwner {
+    function borrowMoney(ERC20 _tokenAddress, address _userAddress, uint256 _count) external onlyOwner {
         _tokenAddress.safeTransfer(_userAddress, _count * (10 ** tokenAddress.decimals()));
     }
 
@@ -143,8 +166,16 @@ contract GrabARedEnvelope is Ownable {
     * @dev 修改随机数地址
     * @param _randomNumberAddress 随机数合约地址
     */
-    function setRandomNumberAddress(RandomNumberConsumer _randomNumberAddress) public onlyOwner {
+    function setRandomNumberAddress(RandomNumberConsumer _randomNumberAddress) external onlyOwner {
         randomNumberConsumer = _randomNumberAddress;
+    }
+
+    /**
+    * @dev 修改奖励token地址
+    * @param _tokenAddress 奖励token地址
+    */
+    function setTokenAddress(ERC20 _tokenAddress) external onlyOwner {
+        tokenAddress = _tokenAddress;
     }
 
 
